f155ba58711aa8dfa5b7110067a4fb574015deca
ASIF\mahmu <Mohammad Mahmudul Islam>
2023-07-25 15:43:24 +0600

    add progress bar

---
+++ copy.js
@@ -0,0 +1,15 @@
+function QuantatiyPlusMinus() {
+    document.addEventListener('click', function(e) {
+      if (e.target.matches('.qtyplus, .qtyminus')) {
+        handleQuantityChange(e.target, e.target.classList.contains('qtyplus') ? 1 : -1);
+      }
+    });
+  
+    function handleQuantityChange(target, value) {
+      const input = document.querySelector('input[name="quantity"]');
+      if (input) {
+        let currentVal = parseInt(input.value) || 0;
+        input.value = Math.max(1, currentVal + value);
+      }
+    }
+  }
